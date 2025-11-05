0046dc60b3e5c035da243703c2a40a711cca3987
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-05 20:08:18 +0530

    new

--- ds.java/array que/src/Main.java
+++ ds.java/array que/src/Main.java
@@ -1,10 +1,10 @@
-////Q1,array sum
+//Q1,array sum
 import java.util.Scanner;
-public static  void main(String[] args) {
+//public static  void main(String[] args) {
 //    Scanner sc = new Scanner(System.in);
 //    System.out.println("Enter size of array:");
 //    int n = sc.nextInt();
-//    int [] arr = new int[n];
+//    int [] arr = new int[n]; //DECLARES ARRAY SIZE
 //    int sum = 0;
 //
 //    System.out.println("Enter array elements:");
@@ -16,6 +16,7 @@ public static  void main(String[] args) {
 
 
 //Q2,find biggest element;
+//public static  void main(String[] args) {
 //    Scanner sc = new Scanner(System.in);
 //    System.out.print("Enter size of array");
 //    int n = sc.nextInt();
@@ -37,23 +38,56 @@ public static  void main(String[] args) {
 
 
 //Q3. Count Even and Odd Numbers
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the size of the array:");
-    int n = sc.nextInt();
-    int [] arr = new int[n];
-    int even = 0;
-    int odd = 0;
+//public static  void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter the size of the array:");
+//    int n = sc.nextInt();
+//    int [] arr = new int[n];
+//    int even = 0;
+//    int odd = 0;
+//
+//    System.out.println("Enter the elements of the array:");
+//    for (int i = 0; i < n; i++) {
+//        arr[i] = sc.nextInt();
+//        if(arr[i]%2==0)
+//            even++;
+//        else
+//            odd++;
+//    }
+//    System.out.println("Even:" + even);
+//    System.out.println("Odd:" + odd);
 
-    System.out.println("Enter the elements of the array:");
-    for (int i = 0; i < n; i++) {
-        arr[i] = sc.nextInt();
-        if(arr[i]%2==0)
-            even++;
-        else
-            odd++;
-    }
-    System.out.println("Even:" + even);
-    System.out.println("Odd:" + odd);
+
+//Q4. Linear Search
+//public static  void main(String[] args) {
+//    Scanner sc = new Scanner (System.in);
+//    System.out.print("Enter the size of the array:");
+//    int n = sc.nextInt();
+//    int[] arr = new int[n];
+//
+//    System.out.println("Enter the elements of the array:");
+//    for (int i = 0; i < n; i++) {
+//        arr[i] = sc.nextInt();
+//    }
+//    System.out.print("Enter number to search:");
+//    int x = sc.nextInt();
+//
+//    boolean found = false;    // Initially assume number is not present
+//
+//    for (int val : arr) {     // Enhanced for-loop → val takes each array element
+//        if (val == x) {       // Compare each value with x
+//            found = true;     // If match is found
+//            // break; <-- If added, stops after first match (optional)
+//        }
+//    }
+//
+//    // Display result
+//    if (found == true) {
+//        System.out.println("Element found");
+//    } else {
+//        System.out.println("Element not found");
+//    }
+
 
 
 
