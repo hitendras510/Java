ed66bae0777caf45a16fa8512f0631abb7fd682a
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-21 16:03:03 +0530

    new

--- ds.java/Interface/src/Main.java
+++ ds.java/Interface/src/Main.java
@@ -1,18 +1,78 @@
-class atm{
-    private int totalamount=1000;
-    public void getb(){
-        System.out.println("Total amount: "+totalamount);
+//class atm{
+//    private int totalamount=1000;
+//    public void getb(){
+//        System.out.println("Total amount: "+totalamount);
+//    }
+//    public void setBalance(int amount){
+//        if(amount>0){
+//            totalamount+=amount;
+//        }
+//    }
+//}
+//class Main{
+//    public static void main(String[] args){
+//        atm s1=new atm();
+//        s1.getb();
+//        s1.setBalance(500);
+//    }
+//}
+//////////////////////////////////////////////////////////////
+class Youtube {
+    private String ChannelName;
+    private int SubscriberCount;
+
+    public Youtube(String ChannelName, int SubscriberCount) {
+        this.ChannelName = ChannelName;
+        this.SubscriberCount = SubscriberCount;
     }
-    public void setBalance(int amount){
-        if(amount>0){
-            totalamount+=amount;
+
+    public void getChannelName() {
+        System.out.println("Channel Name: " + ChannelName);
+    }
+
+    public void getSubscriberCount() {
+        System.out.println("Subscriber Count: " + SubscriberCount);
+    }
+
+    public void increaseSubscriberCount() {
+        SubscriberCount++;
+    }
+
+    public void decreaseSubscriberCount() {
+        if (SubscriberCount > 0) {
+            SubscriberCount--;
+        } else {
+            System.out.println("Error: No subscribers to remove!");
         }
     }
 }
-class Main{
-    public static void main(String[] args){
-        atm s1=new atm();
-        s1.getb();
-        s1.setBalance(500);
+
+public class Main {
+    public static void main(String[] args) {
+        Youtube y1 = new Youtube("Code with Rupesh", 0);
+        y1.getChannelName();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.decreaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.decreaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
+        y1.increaseSubscriberCount();
+        y1.getSubscriberCount();
     }
 }
