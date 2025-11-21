6447d1dad07a35d67cf858f882fa87aa3daf84e4
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-21 15:24:53 +0530

    new

--- ds.java/Interface/src/Main.java
+++ ds.java/Interface/src/Main.java
@@ -1,3 +1,18 @@
-interface A{
-
+class atm{
+    private int totalamount=1000;
+    public void getb(){
+        System.out.println("Total amount: "+totalamount);
+    }
+    public void setBalance(int amount){
+        if(amount>0){
+            totalamount+=amount;
+        }
+    }
+}
+class Main{
+    public static void main(String[] args){
+        atm s1=new atm();
+        s1.getb();
+        s1.setBalance(500);
+    }
 }
