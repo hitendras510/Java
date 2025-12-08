12565fa0760b6a17396d3a47dc26acf40906d40b
Hitendra singh <hitendrasingh5489@gmail.com>
2025-12-08 14:31:57 +0530

    . 

--- iterator/src/Main.java
+++ iterator/src/Main.java
@@ -1,21 +1,42 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-class hello{
+
+//iterator sirf forward travel krta hai
+//class Main{
     public static void main(String[] args){
-        ArrayList<Integer> List = new ArrayList<>();
-        List.add(1);
-        List.add(2);
-        List.add(3);
-        List.add(4);
-        List.add(5);
-        Iterator<Integer> it = List.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
-            //to remove
-            if(it.next() == 5){
-                it.remove();
-            }
-        }
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+//
+//        Iterator<Integer> it = list.iterator();
+//
+//        while(it.hasNext()){
+//            int value = it.next();   // only one next() call
+//            System.out.println(value);
+//
+//            if(value == 5){
+//                it.remove();         // safe removal
+//            }
+//        }
+//        System.out.println("After removal: " + list);
+//    }
+//}
+
+
+//iterator list forward or backward move krskta hai h
+ListIterator<Integer> listIterator = list.listIterator();
+while(listIterator.hasPrevious()){
+    int val =  listIterator.previous();
+    System.out.println(val);
+
+    if(val == 5) {
+        listIterator.remove();
     }
 }
+System.out.println(list);
+    }
